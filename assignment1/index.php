dca3fa9513683c61fdbf8611c81898426b7c5ff0
Yuet Fu Tse <yuettse@hotmail.com>
2015-06-16 16:37:29 -0700

    Create index.php

---
+++ assignment1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Yuet MVP timer</title>
+	<meta charset="UTF-8">
+</head>
+<body>
+<div>
+	<h1>The current time and date:</h1>
+</div>
+<div>
+	<?echo '<h1>' .$timer['monthdayyear'] . '<br>' . $timer['time'] . '</h1>';?>
+</body>
+
+
+</html>
