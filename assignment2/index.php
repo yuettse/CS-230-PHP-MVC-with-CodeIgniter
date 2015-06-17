8bda9a81056c3a9e037c4876157c214d2b3c8023
Yuet Fu Tse <yuettse@hotmail.com>
2015-06-16 21:38:55 -0700

    Create index.php

---
+++ assignment2/index.php
@@ -0,0 +1,33 @@
+<?php 
+	echo $hello['counter'];
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Yuet CodeIgniter Assignment 2</title>
+<meta charset="UTF-8">
+
+
+
+</head>
+
+
+<body>
+<div id="container">
+	<form action="/main/testing"  method='post'>
+		Your Name: <input type='text' name='name'>
+		Comment: <input type='textarea' name='comment'>
+		<input type='submit'>
+	</form>
+</div>
+
+
+
+
+</body>
+
+
+</html>
